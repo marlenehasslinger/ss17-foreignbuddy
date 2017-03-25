8e0e158c8ecdb6112b462e3646a2726f0c03040e
Fleck Marc-Julian <mf122@hdm-stuttgart.de>
2017-03-25 16:09:05 +0100

    Added Fregments to BottomBar + Denye landscape mode

--- android/ForeignBuddy/app/src/main/java/de/hdm_stuttgart/foreignbuddy/MyProfilActivity.java
+++ android/ForeignBuddy/app/src/main/java/de/hdm_stuttgart/foreignbuddy/MyProfilActivity.java
@@ -3,6 +3,7 @@ package de.hdm_stuttgart.foreignbuddy;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -15,6 +16,7 @@ public class MyProfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profil);
 
+        //BottomNavigationFunction
         BottomNavigationView bottomNavigationView =  (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -22,13 +24,23 @@ public class MyProfilActivity extends AppCompatActivity {
                 switch (item.getItemId())
                 {
                     case R.id.action_chats:
-                        Toast.makeText(MyProfilActivity.this, "Chats",Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MyProfilActivity.this, "Chats clicked",Toast.LENGTH_SHORT).show();
+                        ChatsFragment chatsFragment = new ChatsFragment();
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.fragment, chatsFragment);
+                        transaction.addToBackStack(null);
+                        transaction.commit();
                         break;
                     case R.id.action_profil:
-                        Toast.makeText(MyProfilActivity.this, "Profil",Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MyProfilActivity.this, "Profil clicked", Toast.LENGTH_SHORT).show();
+                        ProfilFragment profilFragment = new ProfilFragment();
+                        FragmentTransaction t2 = getSupportFragmentManager().beginTransaction();
+                        t2.replace(R.id.fragment, profilFragment);
+                        t2.addToBackStack(null);
+                        t2.commit();
                         break;
                     case R.id.action_matches:
-                        Toast.makeText(MyProfilActivity.this, "Matches",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MyProfilActivity.this, "Matches clicked",Toast.LENGTH_SHORT).show();
                         break;
                 }
                 return true;
