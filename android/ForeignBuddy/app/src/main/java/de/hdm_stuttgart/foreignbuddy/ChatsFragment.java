c3ae7919a1b1c7589b43f8502efbb27410ef204c
Jan-Niklas Dittrich <jd064@hdm-stuttgart.de>
2017-03-30 12:07:54 +0200

    First try click function

--- android/ForeignBuddy/app/src/main/java/de/hdm_stuttgart/foreignbuddy/ChatsFragment.java
+++ android/ForeignBuddy/app/src/main/java/de/hdm_stuttgart/foreignbuddy/ChatsFragment.java
@@ -5,13 +5,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import static android.R.id.list;
 
 
 public class ChatsFragment extends Fragment {
 
+    ListView chatOverview;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        View view = inflater.inflate(R.layout.fragment_chats, container, false);
+
+        chatOverview = (ListView)view.findViewById(R.id.ChatsOverview);
+
+        /*chatOverview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemSelected(AdapterView parentView, View childView,
+                                       int position, long id)
+            {
+
+            }
+
+            public void onNothingSelected(AdapterView parentView) {
+
+            }
+        });
+
+*/
+        return view;
     }
+
+
+
 }
