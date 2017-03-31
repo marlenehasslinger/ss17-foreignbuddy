85c568d2f8eea06270639d2aebf0db901d2b2cbe
Marlene Hasslinger <mh254@hdm-stuttgart.de>
2017-03-31 13:57:21 +0200

    Kamera Funktion eingebaut, Upload gefixt

--- android/ForeignBuddy/app/src/main/java/de/hdm_stuttgart/foreignbuddy/ProfilFragment.java
+++ android/ForeignBuddy/app/src/main/java/de/hdm_stuttgart/foreignbuddy/ProfilFragment.java
@@ -3,6 +3,7 @@ package de.hdm_stuttgart.foreignbuddy;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -33,6 +34,7 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.io.IOException;
 
 
@@ -43,7 +45,8 @@ public class ProfilFragment extends Fragment implements View.OnClickListener {
 
     private StorageReference storageReference;
     private static final int PICK_IMAGE_REQUEST = 234;
-    private Button btn_choosePhoto, btn_uploadPhoto;
+    static final int CAM_REQUEST =1;
+    private Button btn_choosePhoto, btn_uploadPhoto, btn_takePhoto;
     private ImageView imageView;
     private Uri filepath;
 
@@ -56,8 +59,11 @@ public class ProfilFragment extends Fragment implements View.OnClickListener {
         imageView = (ImageView) view.findViewById(R.id.imageView);
         btn_choosePhoto = (Button) view.findViewById(R.id.btn_choosePhoto);
         btn_uploadPhoto = (Button) view.findViewById(R.id.btn_uploadPhoto);
+        btn_takePhoto = (Button) view.findViewById(R.id.btn_takePhoto);
+
         btn_choosePhoto.setOnClickListener(this);
         btn_uploadPhoto.setOnClickListener(this);
+        btn_takePhoto.setOnClickListener(this);
 
 
        storageReference = FirebaseStorage.getInstance().getReference();
@@ -81,7 +87,16 @@ public class ProfilFragment extends Fragment implements View.OnClickListener {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+        } else if (requestCode ==CAM_REQUEST && resultCode == RESULT_OK){
+            String path ="sdcard/ForeignBuddy/profilepic.jpg";
+            imageView.setImageDrawable(Drawable.createFromPath(path));
+            filepath = Uri.fromFile(new File(path));
+
         }
+
+
+
     }
 
 
@@ -89,28 +104,37 @@ public class ProfilFragment extends Fragment implements View.OnClickListener {
 
         if(filepath != null) {
 
-           final ProgressDialog progressDialog = new ProgressDialog(getApplicationContext()); //?
-            progressDialog.setTitle("Uploading");
-            progressDialog.show();
+          // final ProgressDialog progressDialog = new ProgressDialog(getApplicationContext()); //?
+            //progressDialog.setTitle("Uploading");
+            //progressDialog.show();
 
 
             StorageReference riversRef = storageReference.child("images/profile.jpg");
             riversRef.putFile(filepath)
+
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            progressDialog.dismiss();
-                            Toast.makeText(getApplicationContext(), "File Uploaded", Toast.LENGTH_LONG).show();
+
+                            Log.d("photo", "sucess");
+
+                            //     progressDialog.dismiss();
+                          //  Toast.makeText(getApplicationContext(), "File Uploaded", Toast.LENGTH_LONG).show();
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception exception) {
-                           progressDialog.dismiss();
+                    //       progressDialog.dismiss();
 
-                            Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+                            Log.d("Photo", "fail");
+
+
+                            //  Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_LONG).show();
                         }
                     })
+
+                    /*
             .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
@@ -118,9 +142,12 @@ public class ProfilFragment extends Fragment implements View.OnClickListener {
                     progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
                 }
             })
+           */
 
 
             ;
+
+
         } else {
             //display error toast !!TODO
         }
@@ -135,6 +162,26 @@ public class ProfilFragment extends Fragment implements View.OnClickListener {
 
     }
 
+    public void takePhoto(){
+        Intent camera_Intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = getFile();
+        camera_Intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(camera_Intent, CAM_REQUEST);
+
+    }
+
+    private File getFile() {
+
+        File folder = new File("sdcard/ForeignBuddy");
+
+        if(!folder.exists()){
+            folder.mkdir();
+        }
+
+        File image_file = new File(folder, "profilepic.jpg");
+        return image_file;
+    }
+
 
     @Override
     public void onClick(View view) {
@@ -142,6 +189,8 @@ public class ProfilFragment extends Fragment implements View.OnClickListener {
             showFileChooser();
         } else if (view == btn_uploadPhoto) {
             uploadFile();
+        } else if (view == btn_takePhoto){
+            takePhoto();
         }
     }
 
