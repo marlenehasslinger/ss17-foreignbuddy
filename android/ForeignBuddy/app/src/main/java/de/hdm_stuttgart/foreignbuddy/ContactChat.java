b606f38658f4bf1d4678f454af713852e6fa36de
Jan-Niklas Dittrich <jd064@hdm-stuttgart.de>
2017-04-04 13:21:02 +0200

    Added ChatFRagment onclick to display Chatwindow. Started layout from ChatOverview

---
+++ android/ForeignBuddy/app/src/main/java/de/hdm_stuttgart/foreignbuddy/ContactChat.java
@@ -0,0 +1,44 @@
+package de.hdm_stuttgart.foreignbuddy;
+
+import android.widget.ImageView;
+
+/**
+ * Created by Jan-Niklas on 04.04.17.
+ */
+
+public class ContactChat {
+
+    public String contactname;
+
+    public ImageView contactPicture;
+
+
+    public ContactChat(String contactname, ImageView contactPicture){
+        this.contactname = contactname;
+        this.contactPicture = contactPicture;
+    }
+
+
+
+    public String getContactname() {
+        return contactname;
+    }
+
+    public void setContactname(String contactname) {
+        this.contactname = contactname;
+    }
+
+    public ImageView getContactPicture() {
+        return contactPicture;
+    }
+
+    public void setContactPicture(ImageView contactPicture) {
+        this.contactPicture = contactPicture;
+    }
+
+
+
+
+
+
+}
