9ad9a51c932e3843f9ee9654f956d68d5519073d
Jan-Niklas Dittrich <jd064@hdm-stuttgart.de>
2017-04-18 17:38:03 +0200

    Added Icon, Changed Colors, Added Logo in LogIn

--- android/ForeignBuddy/app/src/main/java/de/hdm_stuttgart/foreignbuddy/Activities/MainActivity.java
+++ android/ForeignBuddy/app/src/main/java/de/hdm_stuttgart/foreignbuddy/Activities/MainActivity.java
@@ -3,8 +3,10 @@ package de.hdm_stuttgart.foreignbuddy.Activities;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import de.hdm_stuttgart.foreignbuddy.Fragments.ChatsFragment;
 import de.hdm_stuttgart.foreignbuddy.Fragments.MatchesFragment;
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.fragment, chat);
