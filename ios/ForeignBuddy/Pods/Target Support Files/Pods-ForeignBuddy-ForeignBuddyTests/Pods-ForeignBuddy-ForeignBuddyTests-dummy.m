5b70c6bd2a6a913479e9a47c0c26087d8ee8c187
Fleck Marc-Julian <mf122@hdm-stuttgart.de>
2017-05-16 12:21:06 +0200

    Again iOS and Firebase Setup

--- ios/ForeignBuddy/Pods/Target Support Files/Pods-ForeignBuddy-ForeignBuddyTests/Pods-ForeignBuddy-ForeignBuddyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ForeignBuddy_ForeignBuddyTests : NSObject
-@end
-@implementation PodsDummy_Pods_ForeignBuddy_ForeignBuddyTests
-@end
--- ios/ForeignBuddy/Pods/Target Support Files/Pods-ForeignBuddy-ForeignBuddyUITests/Pods-ForeignBuddy-ForeignBuddyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ForeignBuddy_ForeignBuddyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ForeignBuddy_ForeignBuddyUITests
-@end
