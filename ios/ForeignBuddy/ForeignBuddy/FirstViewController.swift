28af59f5766681e207722d7862e4df57f80d165c
Fleck Marc-Julian <mf122@hdm-stuttgart.de>
2017-05-16 11:13:30 +0200

    iOS Setup with Firebase

---
+++ ios/ForeignBuddy/ForeignBuddy/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ForeignBuddy
+//
+//  Created by Marc-Julian Fleck on 16.05.17.
+//  Copyright © 2017 Hochschule der Medien. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
