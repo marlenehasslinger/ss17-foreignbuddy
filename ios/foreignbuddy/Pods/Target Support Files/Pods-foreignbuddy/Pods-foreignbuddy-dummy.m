89cbfa8c21d8eb56ad314935616075aaa6f1af49
Fleck Marc-Julian <mf122@hdm-stuttgart.de>
2017-05-16 10:00:03 +0200

    deleted ios

--- ios/foreignbuddy/Pods/Target Support Files/Pods-foreignbuddy/Pods-foreignbuddy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_foreignbuddy : NSObject
-@end
-@implementation PodsDummy_Pods_foreignbuddy
-@end
--- ios/foreignbuddy/Pods/Target Support Files/Pods-foreignbuddyUITests/Pods-foreignbuddyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_foreignbuddyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_foreignbuddyUITests
-@end
